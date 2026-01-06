ab062a721e7aec1bd378c1bc9bd5be34b45e185b
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2026-01-06 16:02:26 +0000

    update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import requests, json
+import requests, json, os
 
 
 def askQuestion(question):
-    API_KEY = "HIDDEN"
     model = "models/gemini-2.5-flash"
-
+    API_KEY = os.environ["G_API_KEY"]
     url = f"https://generativelanguage.googleapis.com/v1beta/{model}:generateContent?key={API_KEY}"
 
     data = {
